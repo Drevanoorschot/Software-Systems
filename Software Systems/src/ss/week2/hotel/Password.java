d3994fc19cc0983f58f9b786749fd67ef98885c2
drevanoorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2016-12-01 13:47:48 +0100

    implement 3.5

--- Software Systems/src/ss/week2/hotel/Password.java
+++ Software Systems/src/ss/week2/hotel/Password.java
@@ -2,12 +2,16 @@ package ss.week2.hotel;
 import ss.week3.pw.*;
 
 public class Password {
-	public static final String INITIAL = "testtest";
+	public static final String INITIAL = "testtest123";
 	public String password;
 	public String factoryPassword;
 	public Checker checker;
+	public Password(Checker checkerInput) {
+		factoryPassword = INITIAL;
+		checker = checkerInput;
+	}
 	public Password() {
-		password = INITIAL;
+		new Password(new BasicChecker());
 	}
 	public boolean acceptable(String suggestion) {
 		return !(suggestion.length() <= 6 || suggestion.contains(" "));
