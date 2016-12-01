ea71306bfe6fe06af2746895c605c8ac75d94d85
drevanoorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2016-12-01 12:15:15 +0100

    enable right checkstyle sheet

--- Software Systems/src/ss/week1/DollarsAndCentsCounter.java
+++ Software Systems/src/ss/week1/DollarsAndCentsCounter.java
@@ -3,21 +3,27 @@ package ss.week1;
 public class DollarsAndCentsCounter {
 
 	public int money;
+
 	public DollarsAndCentsCounter() {
 		money = 0;
 	}
-	public int dollars() {		//dollar count
+
+	public int dollars() { // dollar count
 		return money / 100;
 	}
-	public int cents() {		//cents count
+
+	public int cents() { // cents count
 		return money % 100;
 	}
+
 	public void add(int dollars, int cents) {
-		money = money + dollars * 100 + cents;
+        money = money + dollars * 100 + cents;
 	}
+
 	public void reset() {
 		money = 0;
 	}
+
 	public void substract(int dollars, int cents) {
 		if (dollars < money / 100 && cents < money / 100) {
 			money = money - (dollars * 100 + cents);
