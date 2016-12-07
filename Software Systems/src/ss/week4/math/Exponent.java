0c10b7320ce7fa73b212f652d81350f263356450
drevanoorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2016-12-07 13:47:17 +0100

    implement 4.8

--- Software Systems/src/ss/week4/math/Exponent.java
+++ Software Systems/src/ss/week4/math/Exponent.java
@@ -1,5 +1,21 @@
 package ss.week4.math;
 
-public class Exponent {
+public class Exponent implements Function {
+	private Integer n;
+	
+	public Exponent(Integer exponent) {
+		n = exponent;
+	}
+	
+	@Override
+	public double apply(double c) {
+		return Math.pow(c, n);
+	}
+
+	@Override
+	public Function derivative() {
+		return new LinearProduct(new Exponent(n - 1), new Constant(n));
+	}
+	
 
 }
