1a2eb723450d4368805748bf0d7b325a3977a4c6
drevanoorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2016-12-09 20:30:01 +0100

    implement 4.18

--- Software Systems/src/ss/week4/LinkedList.java
+++ Software Systems/src/ss/week4/LinkedList.java
@@ -2,70 +2,94 @@ package ss.week4;
 
 public class LinkedList<Element> {
 
-    private /*@ spec_public @*/ int size;
-    private Node first;
+	private /* @ spec_public @ */ int size;
+	private Node first;
 
-    //@ ensures \result.size == 0;
-    public LinkedList () {
-        size = 0;
-        first = null;
-    }
+	//// @ ensures \result.size == 0;
+	public LinkedList() {
+		size = 0;
+		first = null;
+	}
 
-    public void add(int index, Element element) {
-        Node newNode = new Node(element);
-        if (index == 0) {
-            newNode.next = first;
-            first = newNode;
-        } else {
-            Node p = getNode(index-1);
-            newNode.next = p.next;
-            p.next = newNode;
-        }
-        size = size + 1;
-    }
+	public void add(int index, Element element) {
+		Node newNode = new Node(element);
+		if (index == 0) {
+			newNode.next = first;
+			first = newNode;
+		} else {
+			Node p = getNode(index - 1);
+			newNode.next = p.next;
+			p.next = newNode;
+		}
+		size = size + 1;
+	}
 
-    //@ ensures this.size == \old(size) - 1;
-    public void remove(Element element) {
-        // TODO: implement, see exercise P-4.18
-    }
+	// @ ensures this.size == \old(size) - 1;
+	public void remove(Element element) {
+		if (get(0) == element) {
+			first = getNode(1);
+			size--;
+			return;
+		}
+		Node node = findBefore(element);
+		for (int i = 0; i < size(); i++) {
+			if (get(i) == element) {
+				node.next = getNode(i + 1);
+				size--;
+			}
+		}
+	}
+	
 
-    public Node findBefore(Element element) {
-    	while (node.)
-    }
+	public Node findBefore(Element element) {
+		if (first.getElement() == element) {
+			return null;
+		}
+		for (int i = 0; i < size(); i++) {
+			if (get(i) == element) {
+				int j = 0;
+				while (element != get(j)) {
+					j++;
+				}
+				return getNode(j - 1);
+			}
+		}
+		return null;
+	}
 
-    //@ requires 0 <= index && index < this.size();
-    public Element get(int index) {
-        Node p = getNode(index);
-        return p.element;
-    }
+	// @ requires 0 <= index && index < this.size();
+	public Element get(int index) {
+		Node p = getNode(index);
+		return p.element;
+	}
 
-    //@ requires 0 <= i && i < this.size();
-    private /*@ pure @*/ Node getNode(int i) {
-        Node p = first;
-        int pos = 0;
-        while (pos != i) {
-            p = p.next;
-            pos = pos + 1;
-        }
-        return p;
-    }
+	// @ requires 0 <= i && i < this.size();
+	private /* @ pure @ */ Node getNode(int i) {
+		Node p = first;
+		int pos = 0;
+		while (pos != i) {
+			p = p.next;
+			pos = pos + 1;
+		}
+		return p;
+	}
 
-    //@ ensures \result >= 0;
-    public /*@ pure @*/ int size() {
-        return size;
-    }
+	// @ ensures \result >= 0;
+	public /* @ pure @ */ int size() {
+		return size;
+	}
 
-    public class Node {
-        private Element element;
-        public Node next;
+	public class Node {
+		private Element element;
+		public Node next;
 
-        public Node(Element element) {
-            this.element = element;
-            this.next = null;
-        }
+		public Node(Element element) {
+			this.element = element;
+			this.next = null;
+		}
 
-        public Element getElement() {
-            return element;
-        }
-    }
+		public Element getElement() {
+			return element;
+		}
+	}
 }
