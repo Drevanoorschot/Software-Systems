909a462c77d9796bee0c44d98798d367a1ca240f
drevanoorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2016-12-15 21:49:11 +0100

    implement 5.1

--- Software Systems/src/ss/week5/MapUtil.java
+++ Software Systems/src/ss/week5/MapUtil.java
@@ -1,39 +1,42 @@
 package ss.week5;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapUtil {
-    public static <K, V> boolean isOneOnOne(Map<K, V> map) {
-        for (int k = 0; k <= map.size(); k++) {
+	public static <K, V> boolean isOneOnOne(Map<K, V> map) {
+		ArrayList<V> list = new ArrayList<>(map.values());
+		Set<V> set = new HashSet<>(map.values());
+		return !(set.size() < list.size() || set.size() < map.size());
 
-        	System.out.println(map);
-        	
-        			
-        }
-        return true;
-    }
-    public static <K, V> 
-           boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
-        // TODO: implement, see exercise P-5.2
-        return false;
-    }
-    public static <K, V> Map<V, Set<K>> inverse(Map<K, V> map) {
-        // TODO: implement, see exercise P-5.3
-        return null;
 	}
+
+	public static <K, V> boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
+		// TODO: implement, see exercise P-5.2
+		return false;
+	}
+
+	public static <K, V> Map<V, Set<K>> inverse(Map<K, V> map) {
+		// TODO: implement, see exercise P-5.3
+		return null;
+	}
+
 	public static <K, V> Map<V, K> inverseBijection(Map<K, V> map) {
-        // TODO: implement, see exercise P-5.3
-        return null;
+		// TODO: implement, see exercise P-5.3
+		return null;
 	}
+
 	public static <K, V, W> boolean compatible(Map<K, V> f, Map<V, W> g) {
-        // TODO: implement, see exercise P-5.4
-        return false;
+		// TODO: implement, see exercise P-5.4
+		return false;
 	}
+
 	public static <K, V, W> Map<K, W> compose(Map<K, V> f, Map<V, W> g) {
-        // TODO: implement, see exercise P-5.5
-        return null;
+		// TODO: implement, see exercise P-5.5
+		return null;
 	}
 }
