b74f0cfd57c212f434c8648f113bb8993632b2a0
Dre <Dre@130.89.161.97>
2016-12-15 12:26:33 +0100

    setup week5

---
+++ Software Systems/src/ss/week5/MapUtil.java
@@ -0,0 +1,39 @@
+package ss.week5;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MapUtil {
+    public static <K, V> boolean isOneOnOne(Map<K, V> map) {
+        for (int k = 0; k <= map.size(); k++) {
+
+        	System.out.println(map);
+        	
+        			
+        }
+        return true;
+    }
+    public static <K, V> 
+           boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
+        // TODO: implement, see exercise P-5.2
+        return false;
+    }
+    public static <K, V> Map<V, Set<K>> inverse(Map<K, V> map) {
+        // TODO: implement, see exercise P-5.3
+        return null;
+	}
+	public static <K, V> Map<V, K> inverseBijection(Map<K, V> map) {
+        // TODO: implement, see exercise P-5.3
+        return null;
+	}
+	public static <K, V, W> boolean compatible(Map<K, V> f, Map<V, W> g) {
+        // TODO: implement, see exercise P-5.4
+        return false;
+	}
+	public static <K, V, W> Map<K, W> compose(Map<K, V> f, Map<V, W> g) {
+        // TODO: implement, see exercise P-5.5
+        return null;
+	}
+}
